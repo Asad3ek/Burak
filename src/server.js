cfd89e48984dca3fea9f002d923d323906ed32a6
Anthony <asadbeksaydullayev2911@gmail.com>
2026-05-23 01:19:32 +0900

    Feat: Initial SetUp

---
+++ src/server.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("BURAK backend has started");
+console.log("Version 1");
+console.log("Version 2");
+const person = "Anthony";
+console.log("Person: ", person);
+//# sourceMappingURL=server.js.map
---
+++ src/server.ts
@@ -0,0 +1,6 @@
+console.log("BURAK backend has started")
+console.log("Version 1");
+console.log("Version 2");
+
+const person: string = "Anthony"
+console.log("Person: ", person)
