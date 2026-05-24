00679ba9e04711b8ed5fd4116201a101582d3edd
Anthony <asadbeksaydullayev2911@gmail.com>
2026-05-24 18:00:30 +0900

    Feat: Connect to the MongoDB via Mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,15 @@
 console.log("BURAK backend has started")
 import dotenv from "dotenv"
 dotenv.config();
+
+//Connection mongoDb via mongoose package
+import mongoose from "mongoose";
+mongoose
+    .connect(process.env.MONGO_URL as string, {})
+    .then(data => {
+        console.log("Successfully connected to MongoDB");
+        const PORT = process.env.PORT ?? 3003;
+    })
+    .catch(err => {
+        console.log("ERROR on connection MongoDB: ", err)
+    })
