05946ac68a35720e3487be7e099db72213fa225d
Anthony <asadbeksaydullayev2911@gmail.com>
2026-06-16 01:40:17 +0900

    Feat: createNewProduct business logic by restaurant member

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from "express"
-import Errors from "../libs/Errors";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 import { T } from "../libs/types/common";
 import ProductService from "../models/product.service";
+import { ProductInput } from "../libs/types/product";
+import { AdminRequest } from "../libs/types/member";
 
 const productService = new ProductService
 const productController: T = {};
+
+/**SPA */
+
+
+/**SSR */
 productController.getAllProducts = (req: Request, res: Response) => {
     try {
         console.log("getAllProducts");
@@ -16,14 +23,28 @@ productController.getAllProducts = (req: Request, res: Response) => {
     }
 }
 
-productController.createNewProduct = (req: Request, res: Response) => {
+productController.createNewProduct = async (req: AdminRequest, res: Response) => {
     try {
         console.log("createNewProduct");
-        res.send("DONE!")
+        if (!req.files?.length)
+            throw new Errors(HttpCode.INTERNAL_SERVICE_ERRROR, Message.CREATE_FAILED);
+
+        const data: ProductInput = req.body;
+        data.productImages = req.files?.map(ele => {
+            return ele.path;
+        })
+
+        await productService.createNewProduct(data);
+
+        res.send(
+            `<script> alert("Successful Creation" window.location.replace(admin/product/all))</script>`
+        )
     } catch (err) {
         console.log("ERROR on createNewProduct", err);
-        if (err instanceof Errors) res.status(err.code).json(err)
-        else res.status(Errors.standard.code).json(Errors.standard)
+        const message = err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
+        res.send(
+            `<script> alert("${message}" window.location.replace(admin/product/all))</script>`
+        );
     }
 }
 
---
+++ src/libs/types/product.ts
@@ -0,0 +1,33 @@
+import { ObjectId } from "mongoose";
+import { ProductCollection, ProductStatus } from "../enums/product.enum";
+
+
+export interface Product {
+    _id: ObjectId;
+    productStatus: ProductStatus,
+    productCollection: ProductCollection,
+    productName: string,
+    productPrice: number,
+    productLeftCount: number,
+    productSize: string,
+    productVolume: number,
+    productDesc?: string,
+    productImages: string[],
+    productViwes: number,
+
+}
+
+
+export interface ProductInput {
+    productStatus?: ProductStatus,
+    productCollection: ProductCollection,
+    productName: string,
+    productPrice: number,
+    productLeftCount: number,
+    productSize?: string,
+    productVolume?: number,
+    productDesc?: string,
+    productImages?: string[],
+    productViwes?: number,
+
+}
