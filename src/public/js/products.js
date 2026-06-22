3032657c1bc433d0314dbc0305e7a0ae79540291
Anthony <asadbeksaydullayev2911@gmail.com>
2026-06-22 20:37:16 +0900

    Feat: Develop UpdateChosenProduct Frontend Logic

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -20,6 +20,27 @@ $(function () {
     $("#cancel-btn").on("click", () => {
         $(".dish-container").slideToggle(100);
         $("#process-btn").css("display", "flex");
+    });
+
+    $(".new-product-status").on("change", async function(e) {
+        const id = e.target.id;
+        const productStatus = $(`#${id}.new-product-status`).val();
+        console.log("productStatus:", productStatus);
+
+        try{
+            const response = await axios.post(`/admin/product/${ id }`, { productStatus: productStatus }); 
+            console.log("response:", response);
+            const result = response.data;
+            if(result.data) {
+                console.log("Product Updated!");
+                $(".new-product-status").blur();
+            } else 
+                alert("Product Update Failed!");
+
+        } catch(err) {
+            console.log(err);
+            alert("Product Update Failed!");
+        }
     })
 })
 
