eaf73d195f4e198252949650ec9887f3848cac60
Anthony <asadbeksaydullayev2911@gmail.com>
2026-07-04 14:58:17 +0900

    feat: develop verify authenticated user credentials

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.post("/member/signup", memberController.signUp);
 router.post("/member/login", memberController.login);
+router.get("/member/detail", memberController.verifyAuth);
 
 
 
