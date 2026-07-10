683ce70538fd4051d00422ae9bca4188148bac06
Anthony <asadbeksaydullayev2911@gmail.com>
2026-07-11 03:54:12 +0900

    feat: develop getMyOrders Rest API

--- src/router.ts
+++ src/router.ts
@@ -44,5 +44,11 @@ router.post(
     "/order/create/",
     memberController.verifyAuth,
     orderController.createOrder
-)
+);
+
+router.get(
+    "/order/all",
+    memberController.verifyAuth,
+    orderController.getMyOrders
+);
 export default router;
