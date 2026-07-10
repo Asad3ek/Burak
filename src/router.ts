cac5603a61f4eeda0b692606238e90978908b9de
Anthony <asadbeksaydullayev2911@gmail.com>
2026-07-11 02:41:33 +0900

    Feat: develop createOrder Rest API

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import express from "express";
 import memberController from "./controllers/member.controller";
 import uploader from "./libs/utils/uploader"
 import productController from "./controllers/product.controller";
+import orderController from "./controllers/order.controller";
 const router = express.Router();
 
 
@@ -38,6 +39,10 @@ router.get(
     productController.getProduct
 )
 
-
 /** Orders API **/
+router.post(
+    "/order/create/",
+    memberController.verifyAuth,
+    orderController.createOrder
+)
 export default router;
